bb96dc425c11f1466a8b915ce39b2405a765249c
Gabriel Ramos <gr2001gabrielgr@gmail.com>
2025-11-21 21:00:26 -0300

    feat: reorganize layout structure for improved readability and maintainability

--- front/src/layouts/AppLayout.tsx
+++ front/src/layouts/AppLayout.tsx
@@ -1,15 +1,19 @@
-import { Outlet, NavLink } from "react-router-dom";
-import "./AppLayout.css";
+import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar/Sidebar";
-
+import Topbar from "../components/Sidebar/Topbar";
+import "./AppLayout.css";
 
 export default function AppLayout() {
-return (
-<div className="app-container">
-<Sidebar />
-<main className="app-main">
-<Outlet />
-</main>
-</div>
-);
+  return (
+    <div className="layout">
+      <Topbar />
+
+      <div className="content-area">
+        <Sidebar />
+        <main className="app-main">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 }
